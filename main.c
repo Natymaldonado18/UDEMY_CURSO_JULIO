47e09730cdf89fab71d4308adf358306fe661676
Natymaldonado18 <129789224+Natymaldonado18@users.noreply.github.com>
2023-07-03 01:25:43 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "blackjack.h"
+#include "operaciones.h"
+#include "persistencia.h"
 
-int main()
-{
-    int opcion;
+//software para gestion de empleados
 
-    menu();
+int main(){
+    
+    struct empleado empleados[100];
+    int opcion = 0;
+    int num_emp = 0;
+
+    num_emp = cargarEmpleados(empleados);
+
+    printf("Bienvenido al gestor de epleados\n");
+    mostrarMenu();
     scanf("%d", &opcion);
 
-    while(opcion != 0) {
-        if (opcion == 1) {
-            jugarPartida();
+    while(opcion !=7){
+        if(opcion == 1){
+            int creado = alta(empleados, num_emp);
+            if(creado == 1){
+                num_emp++;
+            }
         }
-        else if (opcion == 2) {
-            cargarPartidas();
+        else if(opcion == 2){
+            int eliminado = baja(empleados, num_emp);
+            if (eliminado == 1){
+                num_emp--;
+            }
         }
-        fflush(stdin);
-        menu();
-        scanf("%d",&opcion);
+        else if(opcion == 3){
+            listarEmpleados(empleados,num_emp);
+        }
+        else if(opcion == 4){
+            consultarDatosEmpleado(empleados, num_emp);
+        }
+        else if(opcion == 5){
+            modificarSueldo(empleados, num_emp);
+        }
+        else if(opcion == 6){
+            modificarHoras(empleados, num_emp);
+        }
+        mostrarMenu();
+        scanf("%d", &opcion);
     }
-    printf("BYE\n");
+
+    guardarEmpleados(empleados, num_emp);
+    printf("Gracias por su visita.");
 }
